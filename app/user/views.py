439840e4b3ff8e3c098520545c0b6f01013778cf
Nizami Jussupov <nizami.jussupov@nu.edu.kz>
2023-03-01 04:13:49 +0600

    Fixed Linting Errors

--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render # noqa
 from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
