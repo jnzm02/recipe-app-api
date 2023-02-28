fd45e73c2cf143e150f5ecd676f3126c6cd27435
Nizami Jussupov <nizami.jussupov@nu.edu.kz>
2023-03-01 00:46:31 +0600

    Added Super User Creation and Tests

--- app/core/models.py
+++ app/core/models.py
@@ -13,8 +13,19 @@ class UserManager(BaseUserManager):
     """Manager for user profiles"""
     def create_user(self, email, password=None, **extra_fields):
         """Create a new user profile"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,3 +14,31 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertEqual(user.check_password(password), True)
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com']
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'qwerty123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises an error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'qwerty123')
+
+    def test_create_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@exaple.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
