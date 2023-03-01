7449ea5b14e0eb47b978366bf8b41c38429c389a
Nizami Jussupov <nizami.jussupov@nu.edu.kz>
2023-03-02 02:04:32 +0600

    Added new model Recipe and added test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,9 @@
+from decimal import Decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
 
 
 class ModelTests(TestCase):
@@ -42,3 +46,18 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a new recipe"""
+        user = get_user_model().objects.create_user(
+            'test@example.com', 'test123'
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Test recipe',
+            time_minutes=5,
+            price=Decimal(5.500),
+            description='Test description'
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
