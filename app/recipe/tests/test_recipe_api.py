de8221f9b9457d60e37346e5e20374f4096b3c03
Nizami Jussupov <nizami.jussupov@nu.edu.kz>
2023-03-02 03:57:32 +0600

    Added test for testing Post request for creating Recipe

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -96,3 +96,18 @@ class PrivateRecipeAPITests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
+    def test_create_recipe(self):
+        payload = {
+            'title': 'Sample Recipe',
+            'time_minutes': 5,
+            'price': Decimal('29.99'),
+        }
+
+        res = self.client.post(RECIPE_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        recipe = Recipe.objects.get(id=res.data['id'])
+        for k, v in payload.items():
+            self.assertEqual(getattr(recipe, k), v)
+        self.assertEqual(recipe.user, self.user)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -20,3 +20,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return RecipeSerializer
 
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
